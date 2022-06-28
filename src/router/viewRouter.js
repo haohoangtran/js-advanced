2ad91525bcbcec5c565b0b7c36fcb58fe76ba1b1
Hao Hoang Tran <tranhaockpt@gmail.com>
2022-06-28 20:09:04 +0700

    add render task

--- src/router/viewRouter.js
+++ src/router/viewRouter.js
@@ -5,6 +5,21 @@ router.get("/", async (req, res) => {
     if (!req.session.user) {
         return res.render("login", {})
     }
-    res.render("home", {})
+    res.render("home", {
+        tasks: [
+            {
+                title: "Fixbug",
+                done:1
+            },
+            {
+                title: "Nau com",
+                done:0
+            },
+            {
+                title: "Rua bat",
+                done:1
+            }
+        ]
+    })
 })
 module.exports = router
